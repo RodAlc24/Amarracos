4f565460d6030237080ee1ade412e11f0195136f
Rodrigo <ruiaf024@gmail.com>
2024-06-13 20:19:37 +0200

    fixed gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,12 @@ android {
             signingConfig = signingConfigs.getByName("debug")
             isDebuggable = false
         }
+        debug {
+            isMinifyEnabled = false
+            versionNameSuffix = rootProject.extra["debugVersionSuffix"] as String
+            signingConfig = signingConfigs.getByName("debug")
+            isDebuggable = true
+        }
         getByName("debug") {
             versionNameSuffix = rootProject.extra["debugVersionSuffix"] as String
         }
@@ -59,9 +65,9 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -69,11 +75,11 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("androidx.compose.material:material-icons-extended:1.6.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
